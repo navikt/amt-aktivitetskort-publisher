7034c98a6cafaaf65c24fdbb99c1bf19c722bafe
Magnus Evensberget <magnus.evensberget@nav.no>
2023-08-29 15:27:32 +0200

    Autorisering

--- src/main/kotlin/no/nav/amt/aktivitetskort/client/configuration/RestClientConfiguration.kt
+++ src/main/kotlin/no/nav/amt/aktivitetskort/client/configuration/RestClientConfiguration.kt
@@ -4,14 +4,9 @@ import no.nav.amt.aktivitetskort.client.AmtArenaAclClient
 import no.nav.common.token_client.builder.AzureAdTokenClientBuilder
 import no.nav.common.token_client.client.MachineToMachineTokenClient
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
-import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableJwtTokenValidation
@@ -36,6 +31,6 @@ class RestClientConfiguration(
 	@Bean
 	fun amtArenaAclClient(machineToMachineTokenClient: MachineToMachineTokenClient) = AmtArenaAclClient(
 		baseUrl = arenaAclUrl,
-		tokenProvider = { machineToMachineTokenClient.createMachineToMachineToken(arenaAclscope) }
+		tokenProvider = { machineToMachineTokenClient.createMachineToMachineToken(arenaAclscope) },
 	)
 }
