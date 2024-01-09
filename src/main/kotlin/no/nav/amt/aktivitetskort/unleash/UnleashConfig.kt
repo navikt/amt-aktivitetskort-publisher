2076e8c129d8782440d2f2477b9ba3dcfb1160a6
Tia Firing <tia.firing@nav.no>
2024-01-09 09:06:23 +0100

    bli kvitt unleash-feil ved kjøring av tester

--- src/main/kotlin/no/nav/amt/aktivitetskort/unleash/UnleashConfig.kt
+++ src/main/kotlin/no/nav/amt/aktivitetskort/unleash/UnleashConfig.kt
@@ -1,18 +1,21 @@
 package no.nav.amt.aktivitetskort.unleash
 
 import io.getunleash.DefaultUnleash
+import io.getunleash.Unleash
 import io.getunleash.util.UnleashConfig
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
 class UnleashConfig {
 	@Bean
+	@Profile("default")
 	fun unleashClient(
 		@Value("\${app.env.unleashUrl}") unleashUrl: String,
 		@Value("\${app.env.unleashApiToken}") unleashApiToken: String,
-	): DefaultUnleash {
+	): Unleash {
 		val appName = "amt-aktivitetskort-publisher"
 		val config = UnleashConfig.builder()
 			.appName(appName)
