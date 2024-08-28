721c1d52f3453c1fdea38e2bf37956f589d9d0b2
tiafiring <tiafiring@gmail.com>
2024-08-28 10:05:15 +0200

    midlertidig fiks for å ignorere hist-deltakere som mangler arenaid (#184)

--- src/main/kotlin/no/nav/amt/aktivitetskort/service/AktivitetskortService.kt
+++ src/main/kotlin/no/nav/amt/aktivitetskort/service/AktivitetskortService.kt
@@ -13,6 +13,7 @@ import no.nav.amt.aktivitetskort.domain.IKKE_AVTALT_MED_NAV_STATUSER
 import no.nav.amt.aktivitetskort.domain.IdentType
 import no.nav.amt.aktivitetskort.domain.Kilde
 import no.nav.amt.aktivitetskort.domain.LenkeType
+import no.nav.amt.aktivitetskort.domain.ManglerArenaIdException
 import no.nav.amt.aktivitetskort.domain.Melding
 import no.nav.amt.aktivitetskort.repositories.ArrangorRepository
 import no.nav.amt.aktivitetskort.repositories.DeltakerRepository
@@ -83,7 +84,7 @@ class AktivitetskortService(
 		} else if (unleash.isEnabled("amt.enable-komet-deltakere") && isDev()) {
 			return aktivitetskortIdForDeltaker(deltakerId)
 		} else {
-			throw IllegalStateException("Kunne ikke hente aktivitetskortId for deltaker med id $deltakerId")
+			throw ManglerArenaIdException("Kunne ikke hente aktivitetskortId for deltaker med id $deltakerId")
 		}
 	}
 
