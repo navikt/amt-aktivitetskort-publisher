52b6efa19a29df8ce0a0542c051795002f7f3958
Izis Frøyhov <izis.froyhov@nav.no>
2024-12-13 11:35:08 +0100

    Script som gir tilganger

--- src/main/resources/db/migration/V16__grant.sql
+++ src/main/resources/db/migration/V16__grant.sql
@@ -1,11 +1,9 @@
 DO
 $$
-    BEGIN
-        IF EXISTS
-            (SELECT 1 FROM pg_roles WHERE rolname = 'cloudsqliamuser')
-        THEN
-            REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
-            GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
-        END IF;
-    END
+BEGIN
+    IF (SELECT exists(SELECT rolname FROM pg_roles WHERE rolname = 'cloudsqliamuser'))
+    THEN
+        grant all on all tables in schema public to cloudsqliamuser;
+END IF;
+END
 $$;
