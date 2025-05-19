3b101c200d52d66fed538f425695d0fdc074a63a
Tormod Fossum <110398791+tofoss@users.noreply.github.com>
2025-05-19 16:05:14 +0200

    Ny oppgavetekst for felles oppstart (#293)

--- src/main/kotlin/no/nav/amt/aktivitetskort/service/AktivitetskortService.kt
+++ src/main/kotlin/no/nav/amt/aktivitetskort/service/AktivitetskortService.kt
@@ -222,14 +222,31 @@ class AktivitetskortService(
 			return null
 		}
 
+		val (tekst, subtekst) = oppgavetekst(deltaker, arrangor)
+
 		return OppgaveWrapper(
 			ekstern = Oppgave(
-				tekst = "Du har mottatt et utkast til påmelding",
-				subtekst = "Før vi sender dette til ${arrangor.navn} vil vi gjerne at du leser gjennom. " +
-					"Hvis du godkjenner utkastet blir du meldt på, vedtaket fattes og ${arrangor.navn} mottar informasjon.",
+				tekst = tekst,
+				subtekst = subtekst,
 				url = deltaker.deltakerUrl(),
 			),
 			intern = null,
+		)
+	}
+
+	private fun oppgavetekst(deltaker: Deltaker, arrangor: Arrangor): Pair<String, String> = if (deltaker.deltarPaKurs) {
+		Pair(
+			"Du har mottatt et utkast til søknad",
+			"Før søknaden sendes, vil vi gjerne at du leser gjennom. For å avgjøre hvem som skal få plass, " +
+				"kan Nav be om hjelp til vurdering fra arrangøren av kurset. " +
+				"Arrangør eller Nav vil kontakte deg hvis det er behov for et møte. " +
+				"Hvis du godkjenner utkastet blir søknaden sendt inn.",
+		)
+	} else {
+		Pair(
+			"Du har mottatt et utkast til påmelding",
+			"Før vi sender dette til ${arrangor.navn} vil vi gjerne at du leser gjennom. " +
+				"Hvis du godkjenner utkastet blir du meldt på, vedtaket fattes og ${arrangor.navn} mottar informasjon.",
 		)
 	}
 
