e8b788af34ea12720f66c890c364d64f7d9eff4e
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-07-12 12:07:04 +0200

    Refakturert tester

--- src/test/kotlin/no/nav/amt/aktivitetskort/IntegrationTestConfig.kt
+++ src/test/kotlin/no/nav/amt/aktivitetskort/IntegrationTestConfig.kt
@@ -3,15 +3,9 @@ package no.nav.amt.aktivitetskort
 import io.getunleash.FakeUnleash
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 
-@Profile("test")
 @Configuration
 class IntegrationTestConfig {
 	@Bean
-	fun unleashClient(): FakeUnleash {
-		val fakeUnleash = FakeUnleash()
-		fakeUnleash.enableAll()
-		return fakeUnleash
-	}
+	fun unleashClient() = FakeUnleash().apply { enableAll() }
 }
--- src/test/kotlin/no/nav/amt/aktivitetskort/repositories/ArrangorRepositoryTest.kt
+++ src/test/kotlin/no/nav/amt/aktivitetskort/repositories/ArrangorRepositoryTest.kt
@@ -4,58 +4,53 @@ import io.kotest.assertions.fail
 import io.kotest.matchers.shouldBe
 import no.nav.amt.aktivitetskort.IntegrationTest
 import no.nav.amt.aktivitetskort.database.TestData
-import no.nav.amt.aktivitetskort.database.TestDatabaseService
 import no.nav.amt.aktivitetskort.utils.RepositoryResult
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import java.util.UUID
 
 class ArrangorRepositoryTest : IntegrationTest() {
-	@Autowired
-	private lateinit var db: TestDatabaseService
-
 	@Test
 	fun `get(uuid) - finnes - returnerer arrangor`() {
 		val arrangor = TestData.arrangor()
-		db.insertArrangor(arrangor)
-		db.arrangorRepository.get(arrangor.id) shouldBe arrangor
+		testDatabase.insertArrangor(arrangor)
+		testDatabase.arrangorRepository.get(arrangor.id) shouldBe arrangor
 	}
 
 	@Test
 	fun `get(uuid) - finnes ikke - returnerer null`() {
-		db.arrangorRepository.get(UUID.randomUUID()) shouldBe null
+		testDatabase.arrangorRepository.get(UUID.randomUUID()) shouldBe null
 	}
 
 	@Test
 	fun `get(orgnr) - finnes - returnerer arrangor`() {
 		val arrangor = TestData.arrangor()
-		db.insertArrangor(arrangor)
-		db.arrangorRepository.get(arrangor.organisasjonsnummer) shouldBe arrangor
+		testDatabase.insertArrangor(arrangor)
+		testDatabase.arrangorRepository.get(arrangor.organisasjonsnummer) shouldBe arrangor
 	}
 
 	@Test
 	fun `get(orgnr) - finnes ikke - returnerer null`() {
-		db.arrangorRepository.get("foobar") shouldBe null
+		testDatabase.arrangorRepository.get("foobar") shouldBe null
 	}
 
 	@Test
 	fun `upsert - finnes ikke - returnerer Created Result - finnes in database`() {
 		val arrangor = TestData.arrangor()
-		when (val result = db.arrangorRepository.upsert(arrangor)) {
+		when (val result = testDatabase.arrangorRepository.upsert(arrangor)) {
 			is RepositoryResult.Created -> result.data shouldBe arrangor
 			else -> fail("Should be Created, was $result")
 		}
 
-		db.arrangorRepository.get(arrangor.id) shouldBe arrangor
+		testDatabase.arrangorRepository.get(arrangor.id) shouldBe arrangor
 	}
 
 	@Test
 	fun `upsert - finnes - returnerer NoChange Result`() {
 		val arrangor = TestData
 			.arrangor()
-			.also { db.arrangorRepository.upsert(it) }
+			.also { testDatabase.arrangorRepository.upsert(it) }
 
-		when (val result = db.arrangorRepository.upsert(arrangor)) {
+		when (val result = testDatabase.arrangorRepository.upsert(arrangor)) {
 			is RepositoryResult.Created -> fail("Should be NoChange, was $result")
 			is RepositoryResult.Modified -> fail("Should be NoChange, was $result")
 			is RepositoryResult.NoChange -> {}
@@ -66,17 +61,17 @@ class ArrangorRepositoryTest : IntegrationTest() {
 	fun `upsert - endret - returnerer Modified Result og oppdaterer database`() {
 		val initialArrangor = TestData
 			.arrangor()
-			.also { db.arrangorRepository.upsert(it) }
+			.also { testDatabase.arrangorRepository.upsert(it) }
 
 		val updatedArrangor = initialArrangor.copy(
 			navn = "UPDATED",
 		)
 
-		when (val result = db.arrangorRepository.upsert(updatedArrangor)) {
+		when (val result = testDatabase.arrangorRepository.upsert(updatedArrangor)) {
 			is RepositoryResult.Modified -> result.data shouldBe updatedArrangor
 			else -> fail("Should be Modified, was $result")
 		}
 
-		db.arrangorRepository.get(initialArrangor.id) shouldBe updatedArrangor
+		testDatabase.arrangorRepository.get(initialArrangor.id) shouldBe updatedArrangor
 	}
 }
