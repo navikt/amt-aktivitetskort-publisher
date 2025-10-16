79b1883ec0c3dedc4ccae2b841085ec65e25a735
Roar Skinderviken <roar.skinderviken@gmail.com>
2025-10-16 16:15:54 +0200

    Lagt til consumer for tiltakstype

--- src/main/kotlin/no/nav/amt/aktivitetskort/kafka/consumer/KafkaConsumer.kt
+++ src/main/kotlin/no/nav/amt/aktivitetskort/kafka/consumer/KafkaConsumer.kt
@@ -2,9 +2,10 @@ package no.nav.amt.aktivitetskort.kafka.consumer
 
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.amt.aktivitetskort.kafka.consumer.dto.DeltakerlistePayload
+import no.nav.amt.aktivitetskort.kafka.consumer.dto.TiltakstypePayload
+import no.nav.amt.aktivitetskort.repositories.TiltakstypeRepository
 import no.nav.amt.aktivitetskort.service.FeilmeldingService
 import no.nav.amt.aktivitetskort.service.KafkaConsumerService
-import no.nav.amt.aktivitetskort.utils.JsonUtils.fromJson
 import no.nav.amt.lib.utils.objectMapper
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
@@ -14,8 +15,9 @@ import java.util.UUID
 
 @Component
 class KafkaConsumer(
-	val kafkaConsumerService: KafkaConsumerService,
-	val feilmeldingService: FeilmeldingService,
+	private val kafkaConsumerService: KafkaConsumerService,
+	private val feilmeldingService: FeilmeldingService,
+	private val tiltakstypeRepository: TiltakstypeRepository,
 ) {
 	@KafkaListener(
 		topics = [DELTAKER_TOPIC, ARRANGOR_TOPIC, DELTAKERLISTE_TOPIC_V1, DELTAKERLISTE_TOPIC_V2, FEIL_TOPIC],
@@ -25,8 +27,13 @@ class KafkaConsumer(
 		when (record.topic()) {
 			ARRANGOR_TOPIC -> kafkaConsumerService.arrangorHendelse(
 				UUID.fromString(record.key()),
-				record.value()?.let { fromJson(it) },
+				record.value()?.let { objectMapper.readValue(it) },
 			)
+
+			TILTAKSTYPE_TOPIC ->
+				objectMapper
+					.readValue<TiltakstypePayload>(record.value())
+					.let { tiltakstypeRepository.upsert(it.toModel()) }
 
 			// fjernes ved overgang til v2
 			DELTAKERLISTE_TOPIC_V1 ->
@@ -43,13 +50,13 @@ class KafkaConsumer(
 
 			DELTAKER_TOPIC -> kafkaConsumerService.deltakerHendelse(
 				id = UUID.fromString(record.key()),
-				deltaker = record.value()?.let { fromJson(it) },
+				deltaker = record.value()?.let { objectMapper.readValue(it) },
 				offset = record.offset(),
 			)
 
 			FEIL_TOPIC -> feilmeldingService.handleFeilmelding(
 				UUID.fromString(record.key()),
-				record.value()?.let { fromJson(it) },
+				record.value()?.let { objectMapper.readValue(it) },
 			)
 		}
 
