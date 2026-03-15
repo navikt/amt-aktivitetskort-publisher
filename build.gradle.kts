5d4727ee276a511ed443a89d0c8bb358b9135f3e
Roar Skinderviken <roar.skinderviken@gmail.com>
2026-03-15 01:33:37 +0100

    amtLibVersion = "1.2026.03.15_00.13-2aec41852959"

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ val mockkVersion = "1.14.9"
 val commonVersion = "3.2026.03.03_07.58-86d37775258a"
 val tokenSupportVersion = "6.0.1"
 val unleashVersion = "12.2.0"
-val amtLibVersion = "1.2026.03.09_15.52-6a513965cd5d"
+val amtLibVersion = "1.2026.03.15_00.13-2aec41852959"
 val jacksonModuleKotlinVersion = "3.1.0"
 
 dependencies {
