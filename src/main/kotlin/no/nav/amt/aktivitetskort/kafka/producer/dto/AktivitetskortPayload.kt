c2b78c7918bbdcb80d4f8cf9ae136503b6ab346d
Robert Graarup Kittilsen <78416100+robertkittilsen@users.noreply.github.com>
2026-04-08 08:53:06 +0200

    Legger til intern-endepunkt for å kunne slette aktivitetskort  (#413)
    * Legger til intern-endepunkt for å kunne slette aktivitetskort gjennom AKAAS i tilfelle det dukker opp doble aktivitetskort pga. tidligere feil. Ref: https://trello.com/c/2lq12EZI/2708-doble-aktivitetskort

--- src/main/kotlin/no/nav/amt/aktivitetskort/kafka/producer/dto/AktivitetskortPayload.kt
+++ src/main/kotlin/no/nav/amt/aktivitetskort/kafka/producer/dto/AktivitetskortPayload.kt
@@ -2,6 +2,8 @@ package no.nav.amt.aktivitetskort.kafka.producer.dto
 
 import no.nav.amt.aktivitetskort.domain.AktivitetskortTiltakstype
 import no.nav.amt.aktivitetskort.kafka.consumer.SOURCE
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
 import java.util.UUID
 
 data class AktivitetskortPayload(
@@ -10,4 +12,15 @@ data class AktivitetskortPayload(
 	val aktivitetskortType: AktivitetskortTiltakstype,
 	val actionType: String = "UPSERT_AKTIVITETSKORT_V1",
 	val aktivitetskort: AktivitetskortDto,
+)
+
+data class AktivitetskortKasseringPayload(
+	val messageId: UUID,
+	val aktivitetsId: UUID,
+	val actionType: String = "KASSER_AKTIVITET",
+	val personIdent: String,
+	val navIdent: String,
+	val begrunnelse: String,
+	val sendt: ZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC),
+	val source: String = SOURCE,
 )
