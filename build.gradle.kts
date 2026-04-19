2c849ba957f02399efd3481e7c39cfb0fc36e072
Roar Skinderviken <7499642+roar-skinderviken@users.noreply.github.com>
2026-04-19 13:44:47 +0200

    Flyttet versjonsstyring inn i libs.versions.toml. Fikset diverse CVE-warnings. (#419)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,70 +1,60 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-	val kotlinVersion = "2.3.20"
-	id("org.springframework.boot") version "4.0.5"
-	id("io.spring.dependency-management") version "1.1.7"
-	id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
-	kotlin("plugin.spring") version kotlinVersion
-	kotlin("jvm") version kotlinVersion
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
+	alias(libs.plugins.ktlint)
 }
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
 
 repositories {
 	mavenCentral()
 	maven { setUrl("https://github-package-registry-mirror.gc.nav.no/cached/maven-release") }
-	maven { setUrl("https://packages.confluent.io/maven/") }
 }
 
-val logstashEncoderVersion = "9.0"
-val okHttpVersion = "5.3.2"
-val kafkaClientsVersion = "4.2.0"
-val kotestVersion = "6.1.11"
-val klintVersion = "1.4.1"
-val mockkVersion = "1.14.9"
-val commonVersion = "3.2026.04.08_08.37-229807cc181a"
-val tokenSupportVersion = "6.0.5"
-val unleashVersion = "12.2.0"
-val amtLibVersion = "1.2026.04.07_07.09-8e7d69a8ce1a"
-val jacksonModuleKotlinVersion = "3.1.2"
+dependencyManagement {
+	imports {
+		mavenBom(
+			libs.tools.jackson.bom.get().toString(),
+		)
+	}
+}
+
+extra["tomcat.version"] = libs.versions.tomcat.version.get()
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-logging")
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-configuration-processor")
 	implementation("org.springframework.boot:spring-boot-flyway")
 	implementation("org.springframework.boot:spring-boot-kafka")
 
-	implementation("tools.jackson.module:jackson-module-kotlin:$jacksonModuleKotlinVersion")
+	implementation(libs.tools.jackson.module.kotlin)
 
-	implementation("org.apache.kafka:kafka-clients:$kafkaClientsVersion")
+	implementation(libs.kafka.clients)
 
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.postgresql:postgresql")
 
-	implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
+	implementation(libs.okhttp)
 
 	implementation("io.micrometer:micrometer-registry-prometheus")
-	implementation("net.logstash.logback:logstash-logback-encoder:$logstashEncoderVersion")
+	implementation(libs.logstash.encoder)
 
-	implementation("no.nav.common:token-client:$commonVersion")
-	implementation("no.nav.common:rest:$commonVersion")
-	implementation("no.nav.common:log:$commonVersion")
+	implementation(libs.nav.common.token.client)
+	implementation(libs.nav.common.rest)
+	implementation(libs.nav.common.log)
 
-	implementation("no.nav.security:token-validation-spring:$tokenSupportVersion")
+	implementation(libs.token.validation.spring)
 
-	implementation("io.getunleash:unleash-client-java:$unleashVersion")
+	implementation(libs.unleash)
 
-	implementation("no.nav.amt.deltakelser.lib:models:$amtLibVersion")
-	implementation("no.nav.amt.deltakelser.lib:utils:$amtLibVersion")
+	implementation(libs.amt.lib.models)
+	implementation(libs.amt.lib.utils)
 
-	testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
-	testImplementation("io.kotest:kotest-assertions-json-jvm:$kotestVersion")
+	testImplementation(libs.kotest.assertions.core)
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude("com.vaadin.external.google", "android-json")
@@ -73,11 +63,11 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-resttestclient")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 
-	testImplementation("org.testcontainers:testcontainers-postgresql")
-	testImplementation("org.testcontainers:testcontainers-kafka")
+	testImplementation(libs.testcontainers.postgresql)
+	testImplementation(libs.testcontainers.kafka)
 
-	testImplementation("io.mockk:mockk-jvm:$mockkVersion")
-	testImplementation("com.squareup.okhttp3:mockwebserver:$okHttpVersion")
+	testImplementation(libs.mockk)
+	testImplementation(libs.okhttp.mockwebserver)
 }
 
 kotlin {
@@ -94,7 +84,7 @@ kotlin {
 }
 
 ktlint {
-	version = klintVersion
+	version = libs.versions.ktlint.cli.version
 }
 
 tasks.named<Jar>("jar") {
